9866b5ce3d7d7d5946ec9ad6d3a6c9a205366f56
James Westbrook <james.m.westbrook@gmail.com>
2021-11-10 14:02:55 -0500

    init

---
+++ proxy.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/api', createProxyMiddleware({ 
+    target: 'http://localhost:8080', 
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api': '',
+    },
+}));
+
+app.use('*', createProxyMiddleware({ 
+    target: 'http://localhost:3000', 
+    changeOrigin: true,
+}));
+
+app.listen(9090);
